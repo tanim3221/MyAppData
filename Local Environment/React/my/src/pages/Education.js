36dc4124b903753beb4be4345752cc03237008b6
Saklayen Ahmed <77221290+saklayenahmed@users.noreply.github.com>
2023-08-16 17:37:45 +0600

    update data

--- Local Environment/React/my/src/pages/Education.js
+++ Local Environment/React/my/src/pages/Education.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Button, TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody, CircularProgress } from '@mui/material';
+import axios from 'axios';
+import { Container, Button, Dialog, DialogTitle, Snackbar, Box, DialogContent, TextField, TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody, CircularProgress, Stack } from '@mui/material';
 import { Edit } from '@mui/icons-material'
 import { fetchData } from '../components/conn/api';
 import extStyles from '../components/ext/styles.module.css';
 
-function Education() {
+
+function Education({ education, onClose, onChange }) {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [edu, setEducation] = useState({});
+  const [open, setOpen] = useState(false);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
 
   useEffect(() => {
     fetchData()
@@ -19,6 +25,107 @@ function Education() {
         setLoading(false);
       });
   }, []);
+
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleSave = () => {
+    axios.post('http://localhost:8080/saklayen/assets/api/update.php', edu)
+    .then(response => {
+      console.log(response.data);
+      setSnackbarMessage('Changes saved successfully!');
+      setSnackbarOpen(true);
+      const updatedData = data.map(item => {
+        if (item.id === edu.id) {
+          return { ...item, ...edu };
+        }
+        return item;
+      });
+
+      setData(updatedData);
+      setOpen(false);
+    })
+    .catch(error => {
+      console.error(error);
+      setSnackbarMessage('Something went wrong!');
+      setSnackbarOpen(true);
+    });
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setEducation((prevEdu) => ({
+      ...prevEdu,
+      [name]: value
+    }));
+  };
+
+  const renderDialog = () => {
+
+    return (
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>{edu.title}</DialogTitle>
+        <DialogContent>
+          <Box
+            component="form"
+            sx={{
+              '& .MuiTextField-root': { my: 1, mr: 2, width: '25ch' }
+            }}
+          >
+            <TextField
+              label="Rank"
+              name='rank'
+              value={edu.rank}
+              onChange={handleChange}
+            />
+            <TextField
+              label="Degree"
+              name='title'
+              value={edu.title}
+              onChange={handleChange}
+            />
+            <TextField
+              label="Time-Period"
+              name='period'
+              value={edu.period}
+              onChange={handleChange}
+            />
+            <TextField
+              label="Institution"
+              name='institution'
+              value={edu.institution}
+              onChange={handleChange}
+            />
+            <TextField
+              label="Department"
+              name='department'
+              value={edu.department || ''}
+              onChange={handleChange}
+            />
+            <TextField
+              label="Grade"
+              name='grade'
+              value={edu.grade || ''}
+              onChange={handleChange}
+            />
+            <Stack spacing={2} direction="row" style={{ marginTop: '20px' }} justifyContent="flex-start">
+              <Button variant="outlined" onClick={handleClose}>Close</Button>
+              <Button variant="contained" onClick={handleSave}>Save</Button>
+            </Stack>
+          </Box>
+
+        </DialogContent>
+
+      </Dialog>
+    );
+  };
 
   return (
     <Container maxWidth="lg" style={{ marginTop: '2rem' }}>
@@ -49,13 +156,31 @@ function Education() {
                   <TableCell>{item.institution}</TableCell>
                   <TableCell>{item.department}</TableCell>
                   <TableCell>{item.grade}</TableCell>
-                  <TableCell><Button><Edit /></Button></TableCell>
+                  <TableCell>
+                    <Button
+                      onClick={() => {
+                        setEducation(item);
+                        setOpen(true);
+                      }}
+                    >
+                      <Edit />
+                    </Button>
+
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
         )}
       </TableContainer>
+      <Snackbar
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+        open={snackbarOpen}
+        autoHideDuration={3000}
+        onClose={() => setSnackbarOpen(false)}
+        message={snackbarMessage}
+      />
+      {renderDialog()}
     </Container>
   );
 }
