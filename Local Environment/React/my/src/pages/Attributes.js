d2fc58c3d1cad73a2d7bb4d52d55eecbbf3d254f
Saklayen Ahmed <77221290+saklayenahmed@users.noreply.github.com>
2023-08-19 14:56:34 +0600

    attr and int

--- Local Environment/React/my/src/pages/Attributes.js
+++ Local Environment/React/my/src/pages/Attributes.js
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Button, TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody, CircularProgress} from '@mui/material';
-import { Edit } from '@mui/icons-material'
-import { fetchData } from '../components/conn/api';
+import { Container, Button, TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody, Dialog, DialogContent, TextField, CircularProgress, Snackbar, DialogTitle, Box, Stack } from '@mui/material';
+import { Edit, Delete, Check} from '@mui/icons-material'
+import { fetchData, addData, deleteData, updateData } from '../components/conn/api';
 import extStyles from '../components/ext/styles.module.css';
 
-function Others() {
-  const [attribute, setAttribute] = useState([]);
+function Attributes() {
+  const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [mainData, setMainData] = useState({});
+  const [open, setOpen] = useState(false);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+  const [isAdding, setIsAdding] = useState(false);
+  const [dataChanged, setDataChanged] = useState(false);
+
+  const TABLE_NAME = "attributes";
+
 
   useEffect(() => {
     fetchData()
       .then(responseData => {
-        setAttribute(responseData.saklayen.attributes);
+        setData(responseData.saklayen[TABLE_NAME]);
         setLoading(false);
       })
       .catch(error => {
@@ -19,10 +28,166 @@ function Others() {
         setLoading(false);
 
       });
-  }, []);
+  }, [dataChanged]);
+
+  const resetMainDataState = () => {
+    setMainData({});
+  }
+
+  const handleAdd = () => {
+    const requestData = {
+      table: TABLE_NAME,
+      data: mainData
+    };
+    addData(requestData)
+      .then(response => {
+        setSnackbarMessage(response.message);
+        setSnackbarOpen(true);
+        const addData = [...data, mainData];
+        setData(addData);
+        setOpen(false);
+        setIsAdding(false);
+        setMainData({});
+        setDataChanged(!dataChanged);
+      })
+      .catch(error => {
+        console.error(error);
+        setSnackbarMessage(error);
+        setSnackbarOpen(true);
+      });
+  }
+
+  const handleDelete = (id) => {
+    const requestData = {
+      table: TABLE_NAME,
+      id,
+      action: 'delete',
+    };
+
+    deleteData(requestData)
+      .then(response => {
+        setSnackbarMessage(response.message);
+        setSnackbarOpen(true);
+        const deletedData = data.filter(item => item.id !== id);
+        setData(deletedData);
+        setOpen(false);
+      })
+      .catch(error => {
+        console.error(error);
+        setSnackbarMessage(error);
+        setSnackbarOpen(true);
+      });
+  }
+
+  const handleSave = () => {
+
+    const existingData = data.find(item => item.id === mainData.id);
+    const isDataChanged = JSON.stringify(mainData) !== JSON.stringify(existingData);
+
+    if (!isDataChanged) {
+      setSnackbarMessage("No changes to save.");
+      setSnackbarOpen(true);
+      setOpen(false);
+      return;
+    }
+    const requestData = {
+      table: TABLE_NAME,
+      data: mainData
+    };
+
+    updateData(requestData)
+      .then(response => {
+        setSnackbarMessage(response.message);
+        setSnackbarOpen(true);
+        const updatedData = data.map(item => {
+          if (item.id === mainData.id) {
+            return { ...item, ...mainData };
+          }
+          return item;
+        });
+
+        setData(updatedData);
+        setOpen(false);
+      })
+      .catch(error => {
+        console.error(error);
+        setSnackbarMessage(error);
+        setSnackbarOpen(true);
+      });
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+    resetMainDataState();
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setMainData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  }
+
+  // eslint-disable-next-line
+  const renderDialog = () => {
+    return (
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>{isAdding ? 'Add New Attribute' : mainData.title}</DialogTitle>
+        <DialogContent>
+          <Box 
+            component="form"
+            sx={{
+              marginTop: '1rem',
+              display: 'grid',
+              gridTemplateColumns: '1fr 1fr',
+              gap: '1rem'
+            }}
+          >
+            <TextField
+            label="Rank"
+            name="rank"
+            value={mainData.rank}
+            onChange={handleChange}
+            />
+             <TextField
+            label="Title"
+            name="title"
+            value={mainData.title}
+            onChange={handleChange}
+            />
+             <TextField
+            label="Description"
+            name="description"
+            multiline
+            rows={10}
+            value={mainData.description}
+            onChange={handleChange}
+            sx={{ gridColumn: 'span 2' }}
+            />
+            <Stack spacing={2} direction="row" style={{ marginTop: '20px' }} justifyContent="flex-start">
+            {isAdding ? null : <Button style={{backgroundColor:'maroon', color:'white'}} variant="outlined" onClick={() => handleDelete(mainData.id)} ><Delete/></Button>}
+            </Stack>
+            <Stack spacing={2} direction="row" style={{ marginTop: '20px' }} justifyContent="flex-end">
+              <Button variant="outlined" onClick={handleClose}>Close</Button>
+              <Button variant="contained" onClick={isAdding ? handleAdd : handleSave}>{isAdding ? 'Add' : <Check/> }</Button>
+            </Stack>
+            </Box>
+        </DialogContent>
+      </Dialog>
+    );
+  };
 
   return (
-    <Container maxWidth="lg" className={extStyles.container}>
+    <Container maxWidth="lg" style={{ marginTop: '.3rem' }}>
+      <Button
+        variant="contained"
+        onClick={() => {
+          setOpen(true);
+          setIsAdding(true);
+          resetMainDataState();
+        }}
+        style={{ marginBottom: '1.3rem' }}>Add New</Button>
       <TableContainer component={Paper}>
         {loading ? (
           <div className={extStyles.spinnerarea}>
@@ -33,24 +198,43 @@ function Others() {
             <TableHead>
               <TableRow>
                 <TableCell>SL</TableCell>
+                <TableCell>Title</TableCell>
                 <TableCell>Description</TableCell>
                 <TableCell>Action</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {attribute.map(item => (
+              {data.map(item => (
                 <TableRow key={item.id}>
                   <TableCell>{item.rank}</TableCell>
+                  <TableCell>{item.title}</TableCell>
                   <TableCell>{item.description}</TableCell>
-                  <TableCell><Button><Edit /></Button></TableCell>
+                  <TableCell>
+                    <Button
+                      onClick={() => {
+                        setMainData(item);
+                        setOpen(true);
+                        setIsAdding(false)
+                      }}
+                    >
+                      <Edit />
+                    </Button>
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
         )}
       </TableContainer>
+      <Snackbar
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+        open={snackbarOpen}
+        autoHideDuration={3000}
+        onclose={() => setSnackbarOpen(false)}
+        message={snackbarMessage} />
+      {renderDialog()}
     </Container>
   );
 }
 
-export default Others;
+export default Attributes;
