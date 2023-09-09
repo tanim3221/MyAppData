ebdc1aa4218ca0bd3e18e8bf10cd6376c3276bba
Saklayen Ahmed <77221290+saklayenahmed@users.noreply.github.com>
2023-09-09 22:02:40 +0600

    cv update with image

--- Local Environment/React/project-cv/src/pages/Home.js
+++ Local Environment/React/project-cv/src/pages/Home.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 // eslint-disable-next-line
-import { Container, Stack, Box, Snackbar, Card, CardContent, Typography, CardActionArea, List, ListItem, ListItemText, TextField, InputAdornment, Paper, Grid, FormControl, InputLabel, Input, Select, MenuItem, Dialog, Button, DialogContent, DialogTitle, CircularProgress, Divider } from '@mui/material';
-import { Search } from '@mui/icons-material';
+import { Container, Stack, Box, Snackbar, Card, CardContent, Typography, CardActionArea, List, ListItem, ListItemText, TextField, InputAdornment, Paper, Grid, FormControl, InputLabel, Input, Select, MenuItem, Dialog, Button, DialogContent, DialogTitle, CircularProgress, Divider, useMediaQuery, DialogActions, IconButton } from '@mui/material';
+import { Search, Clear, Refresh } from '@mui/icons-material';
 
+import useResponsive from '../utils/UseResponsive';
 import { searchData } from '../auth/api';
 import extStyles from '../utils/styles.module.css';
 
@@ -14,22 +15,45 @@ export default function Home() {
   const [snackbarOpen, setSnackbarOpen] = useState(false);
   const [valueClick, setValueClick] = useState(false);
   const [searchArray, setSearchArray] = useState(false);
+  const [detailsView, setDetailsView] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState('');
   const [searchValue, setSearchValue] = useState('');
   const [searchResult, setSearchResult] = useState([]);
   const [searchSingle, setSearchSingle] = useState({});
+  const isDesktop = useResponsive('up', 'lg');
+  const isSmallScreen = useMediaQuery((theme) => theme.breakpoints.down('sm'));
+  const dialogMinWidth = isSmallScreen ? '90%' : '500px';
+  const [imageUrl, setImageUrl] = useState(null);
+  const [currentExtIndex, setCurrentExtIndex] = useState(0);
+  const regNo = searchSingle.reg_no;
+  const imageExtensions = ['jpeg', 'jpg', 'png'];
 
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
     }, 1000);
-  }, []);
 
-  const handleSearchChange = (event) => {
-    const searchKey = event.target.value;
-    setSearchValue(searchKey);
-  };
+    const loadImage = async () => {
+      try {
+        const ext = imageExtensions[currentExtIndex];
+        const potentialUrl = `https://s3.brilliant.com.bd/icab-exam/studentDocument/${regNo}/${regNo}.${ext}`;
+        const imageExists = await checkIfImageExists(potentialUrl);
 
+        // console.log(potentialUrl);
+
+        if (imageExists) {
+          setImageUrl(potentialUrl);
+        } else {
+          const nextExtIndex = (currentExtIndex + 1) % imageExtensions.length;
+          setCurrentExtIndex(nextExtIndex);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    loadImage();
+  }, [regNo, currentExtIndex]);
 
   const handleSearch = () => {
     const requestData = {
@@ -61,24 +85,122 @@ export default function Home() {
     }
   };
 
+  const handleClose = () => {
+    setDetailsView(false);
+  }
 
+  const handleImgError = (e) => {
+    const imagePath = process.env.PUBLIC_URL + '/android-chrome-192x192.png';
+    e.target.src = imagePath;
+  }
 
+  const handleExtensionChange = () => {
+    const nextExtIndex = (currentExtIndex + 1) % imageExtensions.length;
+    setCurrentExtIndex(nextExtIndex);
+  };
+
+  async function checkIfImageExists(url) {
+    try {
+      const response = await fetch(url, {
+        mode: 'no-cors',
+      });
+
+      if (response.status === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      return false;
+    }
+  }
+
+  function DetailsInfoView() {
+
+    if (imageUrl) {
+      return (
+        <Card key={searchSingle.id}>
+          <CardContent style={{ display: 'flex', alignItems: 'flex-start' }}>
+            <div style={{
+              marginRight: '1rem'
+            }} >
+              <img
+                src={imageUrl}
+                alt={`${searchSingle.name}'s profile picture`}
+                style={{ borderRadius: '.7rem', width: '130px', height: '100%', objectFit: 'cover' }}
+                onError={handleImgError}
+              />
+            </div>
+            <div>
+              <Typography variant="h6" component="div">
+                {searchSingle.name} ({searchSingle.reg_no})
+              </Typography>
+              <Typography variant="body2" color="textSecondary">
+                <strong>Mobile:</strong> {searchSingle.mobile}
+              </Typography>
+              <Typography variant="body2" color="textSecondary">
+                <strong>Email:</strong> {searchSingle.email}
+              </Typography>
+              <Typography variant="body2" color="textSecondary">
+                <strong>Birthday:</strong> {searchSingle.birthday}
+              </Typography>
+              <IconButton style={{
+                position: 'absolute',
+                right: '.5rem',
+                bottom: '.4rem'
+              }} onClick={handleExtensionChange}>
+                <Refresh />
+              </IconButton>
+            </div>
+          </CardContent>
+        </Card>
+      );
+    } else {
+      return <div className={extStyles.spinnerarea_deatails}><CircularProgress /></div>;
+    }
+  }
+
+  // eslint-disable-next-line
+  const renderDetailsDialog = () => {
+    return (
+      <Dialog
+        maxWidth="md"
+        PaperProps={{
+          sx: {
+            minWidth: dialogMinWidth,
+          },
+        }}
+        open={detailsView}
+        onClose={() => setDetailsView(false)}
+      >
+        <DialogTitle>{searchSingle.name}</DialogTitle>
+        <DialogContent>
+          <DetailsInfoView />
+        </DialogContent>
+        <DialogActions sx={{
+          justifyContent: 'flex-end',
+          paddingRight: '1rem',
+          paddingBottom: '1rem'
+        }}>
+          <Button variant="outlined" onClick={handleClose}>Close</Button>
+        </DialogActions>
+      </Dialog>
+    )
+  };
   const LeftSide = () => (
-    <div style={{
-      height: 'calc(100vh)', // Adjust the height as needed
-      overflowY: 'auto',
-    }}>
+    <div>
       <Paper style={{
         padding: '1rem',
-        borderRadius: '1rem'
+        borderRadius: '1rem',
+        position: 'relative',
       }}>
-        {/* Fixed header for search */}
         <div style={{
           position: 'sticky',
-          top: 0,
+          top: '0',
           background: 'white',
           zIndex: 1,
-          padding: '1rem 0',
+          padding: '1rem',
+          borderRadius: '1rem',
         }}>
           <Box display="flex" alignItems="center">
             <Input
@@ -91,7 +213,20 @@ export default function Home() {
               }}
               name='search'
               value={searchValue}
-              onChange={handleSearchChange}
+              onChange={(e) => setSearchValue(e.target.value)}
+              endAdornment={
+                <InputAdornment position="end">
+                  {searchValue && (
+                    <Clear
+                      onClick={() => setSearchValue('')}
+                      style={{
+                        cursor: 'pointer',
+                        marginRight: '.5rem'
+                      }}
+                    />
+                  )}
+                </InputAdornment>
+              }
             />
             <Button variant="contained" color="primary" onClick={handleSearch}>
               <Search />
@@ -99,20 +234,25 @@ export default function Home() {
           </Box>
         </div>
         {searchArray ? (
-          <List>
+          <List style={{
+            overflow: 'scroll',
+            height: '30rem'
+          }}>
             {searchResult.map((item) => (
               <ListItem key={item.id} sx={{
-                padding: '.5rem 0rem'
-              }}
-                onClick={() => {
-                  setSearchSingle(item)
-                  setValueClick(true)
-                }}
-              >
+                padding: '.3rem 0rem'
+              }}>
                 <Card sx={{
                   width: '100%',
-                  padding: '0px'
-                }}>
+                  padding: '0px',
+                }}
+                  onClick={() => {
+                    setSearchSingle(item)
+                    setValueClick(true)
+                    { isDesktop ? setDetailsView(false) : setDetailsView(true) }
+
+                  }}
+                >
                   <CardActionArea sx={{
                     padding: '0px'
                   }}>
@@ -133,38 +273,19 @@ export default function Home() {
             No Search Result.
           </div>
         )}
-
-
       </Paper>
     </div>
   );
 
+
   const RightSide = () => (
-    <div style={{
-      height: 'calc(100vh - 64px)', // Adjust the height as needed
-      overflowY: 'auto',
-    }}>
+    <div>
       <Paper style={{
         padding: '0px',
         borderRadius: '1rem'
       }}>
         {valueClick ? (
-          <Card key={searchSingle.id} style={{ marginBottom: '16px' }}>
-            <CardContent>
-              <Typography variant="h6" component="div">
-                {searchSingle.name} ({searchSingle.reg_no})
-              </Typography>
-              <Typography variant="body2" color="textSecondary">
-                <strong>Mobile:</strong> {searchSingle.mobile}
-              </Typography>
-              <Typography variant="body2" color="textSecondary">
-                <strong>Email:</strong> {searchSingle.email}
-              </Typography>
-              <Typography variant="body2" color="textSecondary">
-                <strong>Birthday:</strong> {searchSingle.birthday}
-              </Typography>
-            </CardContent>
-          </Card>
+          <DetailsInfoView />
         ) : (
           <div className={extStyles.not_selected} >No data selected.</div>
         )}
@@ -174,20 +295,25 @@ export default function Home() {
 
 
   return (
-      <Container maxWidth="xl">
+    <Container maxWidth="xl">
+      <>
         {loading ? (
           <div className={extStyles.spinnerarea}>
             <CircularProgress />
           </div>
         ) : (
           <Grid container spacing={2}>
-            <Grid item xs={5}>
+            <Grid item xs={12} lg={5} md={12} sm={12}>
               <LeftSide />
             </Grid>
+            {isDesktop ? (
+              <Grid item xs={12} lg={7} md={12} sm={12}>
+                <RightSide />
+              </Grid>
+            ) : (
+              <></>
+            )}
 
-            <Grid item xs={7}>
-              <RightSide />
-            </Grid>
           </Grid>
         )}
 
@@ -198,7 +324,8 @@ export default function Home() {
           onClose={() => setSnackbarOpen(false)}
           message={snackbarMessage.toString()}
         />
-      </Container>
-
+        {!isDesktop ? renderDetailsDialog() : ""}
+      </>
+    </Container>
   );
 }
