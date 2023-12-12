b7dd8bb5228893c14662a63478e3b780955a9746
Saklayen Ahmed <77221290+saklayenahmed@users.noreply.github.com>
2023-12-12 23:38:50 +0600

    aehfs kjda

---
+++ Local Environment/Python/Udemy-Python/QuizGameUI/data.py
@@ -0,0 +1,20 @@
+import requests
+
+
+class Data:
+
+    def  __init__(self):
+        self.api = "https://opentdb.com/api.php"
+        self.param = {
+            "amount": 10,
+            "type": "boolean",
+        }
+
+
+    def get_questions(self):    
+        response = requests.get(url=self.api, params=self.param)
+        response.raise_for_status()
+        if response.status_code == 200:
+            new_q = response.json()
+            return new_q
+            
---
+++ Local Environment/Python/Udemy-Python/QuizGameUI/main.py
@@ -0,0 +1,30 @@
+from data import Data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+
+api_q = Data()
+
+data = api_q.get_questions()
+questions = data['results']
+question_data = []
+for q in questions:
+    question_data.append({
+        "text":q['question'],
+        "answer":q['correct_answer']
+    })
+
+question_bank = []
+for question in question_data:
+    question_text = question['text']
+    question_answer = question['answer']
+    new_question = Question(question_text,question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You have completed the quiz.")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}.")
