3f0603657334553b8908a694146c37986760a5a6
Saklayen Ahmed <77221290+saklayenahmed@users.noreply.github.com>
2023-12-12 17:12:03 +0600

    Update main.normal.py

--- Local Environment/Python/Udemy-Python/SMTP/main.normal.py
+++ Local Environment/Python/Udemy-Python/SMTP/main.normal.py
@@ -14,7 +14,6 @@ data = pd.read_csv("./birthdays.csv")
 
 birthdays_dict = {(data_row['month'],data_row['day']): data_row for (index, data_row) in data.iterrows()}
 
-
 if today_tuple in birthdays_dict:
     birthday_person = birthdays_dict[today_tuple]
     file_path = f"./letter_templates/letter_{rd.randint(1,3)}.txt"
@@ -22,8 +21,7 @@ if today_tuple in birthdays_dict:
         contents = letter_file.read()
         contents = contents.replace("[NAME]", birthday_person['name'])
 
-
-    with smtp.SMTP(HOST) as conn:
+    with smtp.SMTP(HOST, 587) as conn:
         conn.starttls()
         conn.login(MY_MAIL, MY_PASSWORD)
         conn.sendmail(
