e167f57be3349e13253f7000c1f737e395e79332
Saklayen Ahmed <77221290+saklayenahmed@users.noreply.github.com>
2023-12-24 15:52:36 +0600

    apowegfejkas

--- Local Environment/Python/Udemy-Python/FlaskWeb/cafe_api.py
+++ Local Environment/Python/Udemy-Python/FlaskWeb/cafe_api.py
@@ -25,22 +25,81 @@ class Cafe(db.Model):
     coffee_price = db.Column(db.String(250), nullable=True)
 
 
+    def to_dict(self):
+        dictionary ={}
+        for column in self.__table__.columns:
+            dictionary[column.name] = getattr(self,column.name)
+        return dictionary
+        # return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+
 with app.app_context():
     db.create_all()
-
-def to_dict(self):
-    dictionary ={}
-    for column in self.__table__.columns:
-        dictionary[column.name] = getattr(self,column.name)
-    return dictionary
     
-    # return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+@app.route("/add", methods=["POST"])
+def post_new_cafe():
+    new_cafe = Cafe (
+            name=request.form.get("name"),
+            map_url=request.form.get("map_url"),
+            img_url=request.form.get("img_url"),
+            location=request.form.get("loc"),
+            has_sockets=bool(request.form.get("sockets")),
+            has_toilet=bool(request.form.get("toilet")),
+            has_wifi=bool(request.form.get("wifi")),
+            can_take_calls=bool(request.form.get("calls")),
+            seats=request.form.get("seats"),
+            coffee_price=request.form.get("coffee_price"),
+            )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response = {"success": "Successfully added the new cafe."})
+    
 
 @app.route("/all")
 def get_all_cafe():
     result = db.session.execute(db.select(Cafe).order_by(Cafe.name))
     all_cafes = result.scalars().all()
-    return jsonify(cafes=[cafe.dict() for cafe in all_cafes])
+    return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
+
+
+@app.route("/update-price/<cafe_id>", methods=["PATCH"])
+def patch_new_price(cafe_id):
+    new_price = request.args.get("new_price")
+    cafe = db.get_or_404(Cafe,cafe_id)
+    if cafe:
+        cafe.coffee_price = new_price
+        db.session.commit()
+        return jsonify(response = {"success":"Succefully updated the price."})
+    else:
+        return jsonify(error={"Not Found":"Sorry a cafe with that id was not found in the database."})
+        
+
+@app.route("/search", methods=["GET"])
+def get_cafe_at_location():
+    q = request.args.get("loc")
+    result = db.session.execute(db.select(Cafe).where(Cafe.location==q))
+    all_cafes = result.scalars().all()
+    
+    if all_cafes:
+        return jsonify(cafes = [cafe.to_dict() for cafe in all_cafes])
+    else:
+        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."}), 404
+
+
+@app.route("/report-closed/<int:cafe_id>", methods=["DELETE"])
+def delete_cafe(cafe_id):
+    api_key = request.args.get("api-key")
+    if api_key == "secret":
+        cafe = db.get_or_404(Cafe,cafe_id)
+        if cafe:
+            db.session.delete(cafe)
+            db.session.commit()
+            return jsonify(response= {"success": "Succefully deleted the cafe from the database."})
+        else:
+            return jsonify(error= {"Not Found": "Sorry a cafe with that id was not found in the database."})
+    else:
+        return jsonify(error= {"Forbidden": "Sorry, that's not allowed. Make sure you have the correct api_key."})
 
 
 @app.route("/random")
@@ -53,16 +112,6 @@ def get_random_cafe():
 @app.route("/")
 def home():
     return render_template("cafe_api/index.html")
-
-
-# HTTP GET - Read Record
-
-# HTTP POST - Create Record
-
-# HTTP PUT/PATCH - Update Record
-
-# HTTP DELETE - Delete Record
-
 
 if __name__ == '__main__':
     app.run(debug=True)
