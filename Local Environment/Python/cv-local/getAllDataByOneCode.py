ac5fa3621480c542a883bb5659b302436def4d38
Saklayen Ahmed <77221290+saklayenahmed@users.noreply.github.com>
2023-12-24 23:22:48 +0600

    python blog api

--- Local Environment/Python/cv-local/getAllDataByOneCode.py
+++ Local Environment/Python/cv-local/getAllDataByOneCode.py
@@ -9,6 +9,72 @@ os.system('cls' if os.name == 'nt' else 'clear')
 # Disable SSL verification warnings
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+RESULT_API = "https://exam.icab.org.bd/api/v1/ExamRegs/get-grade-sheet-single"
+
+GET_EXAM_DETAILS = "https://exam.icab.org.bd/api/v1/ExamRegs/GetExamRegByExamlevelMonthidSessionyearRegno"
+
+token = open("auth_token.txt", "r")
+auth_token = token.read()
+headers = {
+    'Authorization': auth_token,
+    'Content-Type': 'application/json',  # Adjust this as needed
+}
+
+def get_result(reg_no, exam):
+    try:
+        roll_no = get_roll_no(reg_no, exam)
+        if not roll_no:
+            print("Error getting roll number.")
+            return
+
+        payload = {
+            "monthId": "2",
+            "examLevel": exam,
+            "sessionYear": "2023",
+            "rollNo": roll_no
+        }
+
+        response = requests.post(RESULT_API, json=payload, headers=headers, verify=False)
+        if response.status_code == 200:
+            formatted_response = json.dumps(response.json()['payload'], indent=4)
+            print(formatted_response)
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+
+    except requests.RequestException as e:
+        print(f"Request error: {e}")
+    except json.JSONDecodeError as e:
+        print(f"JSON decoding error: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+def get_roll_no(reg_no, exam):
+    try:
+        payload = {
+            "examLevel": exam,
+            "monthId": "2",
+            "sessionYear": "2023",
+            "regNo": reg_no
+        }
+
+        response = requests.post(GET_EXAM_DETAILS, json=payload, headers=headers, verify=False)
+        if response.status_code == 200:
+            data = response.json()
+            roll_no = data.get('payload', {}).get('examRollno')
+            return roll_no
+        else:
+            print(f"Error getting roll number: {response.status_code} - {response.text}")
+            return None
+
+    except requests.RequestException as e:
+        print(f"Request error: {e}")
+        return None
+    except json.JSONDecodeError as e:
+        print(f"JSON decoding error: {e}")
+        return None
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return None
 
 def get_exam_admit(roll_no):
     # API endpoint
@@ -21,24 +87,9 @@ def get_exam_admit(roll_no):
         "rollNo": roll_no
     }
 
-
-    # Authorization token
-    token = open("auth_token.txt", "r")
-    auth_token = token.read()
-
-    # Set the headers with the Authorization token
-    headers = {
-        'Authorization': auth_token,
-        'Content-Type': 'application/json',  # Adjust this as needed
-    }
-
     try:
-        # Sending POST request to the API endpoint
         response = requests.post(api_url_admit, json=payload_admit, headers=headers, verify=False)
-
-        # Check if the request was successful (status code 200)
         if response.status_code == 200:
-            # Pretty print the JSON response
             formatted_response = json.dumps(response.json(), indent=4)
             print(formatted_response)
         else:
@@ -56,16 +107,6 @@ def get_exam_registration(reg_no):
         "monthId": 2,
         "sessionYear": "2023",
         "regNo": reg_no
-    }
-
-    # Authorization token
-    token = open("auth_token.txt", "r")
-    auth_token = token.read()
-
-    # Set the headers with the Authorization token
-    headers = {
-        'Authorization': auth_token,
-        'Content-Type': 'application/json',  # Adjust this as needed
     }
 
     try:
@@ -94,16 +135,6 @@ def get_formfillup(reg_no, exam, month,year):
         "sessionYear": year
     }
 
-    # Authorization token
-    token = open("auth_token.txt", "r")
-    auth_token = token.read()
-
-    # Set the headers with the Authorization token
-    headers = {
-        'Authorization': auth_token,
-        'Content-Type': 'application/json',  # Adjust this as needed
-    }
-
     try:
         # Sending POST request to the API endpoint
         response = requests.post(url, json=payload, headers=headers, verify=False)
@@ -123,6 +154,7 @@ intinput = input(
         '''1. Student Roll No\n'''
         '''2. Student Admit Card\n'''
         '''3. Form Fillup Info\n'''
+        '''4. Get Result\n'''
         '''Enter your choice: '''
 )
 
@@ -132,6 +164,11 @@ if intinput=="1":
 elif intinput=="2":
     roll_no = input("Enter RollNo: ")
     get_exam_admit(roll_no)
+elif intinput=="4":
+    while True:
+        exam = input("61. CERTIFICATE LEVEL, 62. PROFESSIONAL LEVEL, 63. ADVANCED LEVEL : ")
+        reg_no = input("Enter regNo: ")
+        get_result(reg_no, exam)
 elif intinput=="3":
     regNo = input("Enter regNo: ")
     exam = input("61. CERTIFICATE LEVEL, 62. PROFESSIONAL LEVEL, 63. ADVANCED LEVEL : ")
